a37d4defe65c37a4c5d17eb1c128c96017771aee
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-06 13:36:00 +0100

    add: attacker weapon tech functions and tests

--- spec/combatSimulatorSpec.js
+++ spec/combatSimulatorSpec.js
@@ -27,3 +27,31 @@ describe('defenderWeaponTech', function() {
   })
 
 });
+
+describe('attackerWeaponTech', function() {
+  let attackerTechLevel;
+  beforeEach(function() {
+    attackerTechLevel = new AttackerTechLevel();
+  });
+
+  it('starts at level 1', function() {
+    expect(attackerTechLevel.getCurrentLevel()).toEqual(1);
+  });
+
+  it('increases in level with levelUp', function() {
+    attackerTechLevel.levelUp();
+    expect(attackerTechLevel.getCurrentLevel()).toEqual(2);
+  })
+
+  it('descreases in level with levelDown', function() {
+    attackerTechLevel.levelUp();
+    attackerTechLevel.levelDown();
+    expect(attackerTechLevel.getCurrentLevel()).toEqual(1);
+  })
+
+  it('has a minimum level of 1', function() {
+    attackerTechLevel.levelDown();
+    expect(attackerTechLevel.getCurrentLevel()).toEqual(1);
+  })
+
+});
--- src/combatSimulator.js
+++ src/combatSimulator.js
@@ -26,3 +26,30 @@ isMinimumLevel() {
 };
 
 }
+
+class AttackerTechLevel {
+  constructor() {
+    this.MINIMUM_LEVEL = 1;
+    this.level = 1;
+  };
+
+getCurrentLevel() {
+  return this.level;
+};
+
+levelUp() {
+  this.level += 1
+};
+
+levelDown() {
+  if (this.isMinimumLevel()) {
+    return;
+  }
+  this.level -= 1
+};
+
+isMinimumLevel() {
+  return this.level === this.MINIMUM_LEVEL;
+};
+
+}
