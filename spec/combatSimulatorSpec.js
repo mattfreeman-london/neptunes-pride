b366363ef88514c1d113f764d58551f60b5c71b5
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-06 13:14:37 +0100

    add: level up and level down tests

--- spec/combatSimulatorSpec.js
+++ spec/combatSimulatorSpec.js
@@ -1,12 +1,24 @@
 'use strict'
 
-describe('defenderWeaponTech', funtion() {
+describe('defenderWeaponTech', function() {
   let defenderTechLevel;
   beforeEach(function() {
     defenderTechLevel = new DefenderTechLevel();
-  })
-})
+  });
 
-it('starts at level 1', function() {
-  expect(defenderTechLevel.getCurrentLevel()).toEqual(1);
+  it('starts at level 1', function() {
+    expect(defenderTechLevel.getCurrentLevel()).toEqual(1);
+  });
+
+  it('increases in level with levelUp', function() {
+    defenderTechLevel.levelUp();
+    expect(defenderTechLevel.getCurrentLevel()).toEqual(2);
+  })
+
+  it('descreases in level with levelDown', function() {
+    defenderTechLevel.levelUp();
+    defenderTechLevel.levelDown();
+    expect(defenderTechLevel.getCurrentLevel()).toEqual(1);
+  })
+
 });
