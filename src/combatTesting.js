30dfc59ba2fab0c444bb01be1bcd5505c47ebe46
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-06 12:43:55 +0100

    add: get current level function

---
+++ src/combatTesting.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class DefenderTechLevel {
+  constructor() {
+    this.MINIMUM_LEVEL = 1;
+    this.level = 1;
+    this.DEFAULT_LEVEL = 1;
+  };
+
+getCurrentLevel() {
+  return this.level;
+};
+
+}
