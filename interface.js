2dd21385d59a85d77ccbd679a568c574637922b6
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-04-07 15:05:21 +0100

    cleared: index and interface files as not currently relevant

--- interface.js
+++ interface.js
@@ -1,19 +1,1 @@
-$(document).ready(function() {
-  let defenderTechLevel = new DefenderTechLevel();
-  updateLevel();
 
-  $('#level-up').on('click', function() {
-    defenderTechLevel.levelUp();
-    updateLevel();
-  })
-
-  $('#level-down').on('click', function() {
-    defenderTechLevel.levelDown();
-    updateLevel();
-  })
-
-  function updateLevel() {
-    $('#defender-tech-level').text(defenderTechLevel.level);
-  };
-
-})
